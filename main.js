41900ef88c9491740d1c8af16768da628434f894
AmanturShamurzaev <amanturshamurzaev@gmail.com>
2022-08-02 17:47:42 +0600

    hw3

---
+++ main.js
@@ -0,0 +1,87 @@
+// 1
+// // let salaries = {
+// //     John: 100,
+// //     Ann: 160,
+// //     Pete: 130
+// // }
+// // let sum = 0; for (let key in salaries)
+// // {sum += salaries[key]};
+// // console.log(sum)
+
+
+// 2
+//
+// let obj = {
+//     name: 'Ivan',
+//     age: 26,
+// }
+// console.log(Object.values(name))
+
+// 3
+
+// let obj2 = {
+//     name: 'Ivan',
+//     age: 26,
+// }
+// obj2.age = 31
+// console.log(obj2)
+
+4
+
+// let obj2 = {
+//     name: 'Ivan',
+//     age: 26,
+//     student: true,
+// }
+// obj2.student = false
+// console.log(obj2)
+//
+// // 5
+// let obj = {name:ABRACADABRA', age: 15, student: true}
+
+// 6
+// let obj = {
+//     name: 'Ivan',
+//     age: 26,
+//     student: true,
+// }
+
+// 7
+
+// let obj = {
+//     name: 'Ivan',
+//     age: 26,
+//     student: true,
+// }
+// let obj2 = JSON.parse(JSON.stringify(obj))
+// console.log(obj2)
+
+// 8
+
+// let cars = ['bmw', 'honda','mers','lexus']
+// cars.pop()
+// console.log(cars)
+//
+// 9
+// let cars = ['bmw', 'honda','mers','lexus']
+// console.log(cars[3])
+
+// 10
+// let cars = ['bmw', 'honda','mers','lexus']
+// cars[1] = 'Toyota'
+// console.log(cars);
+
+// 11
+// let cars = ['bmw', 'honda','mers','lexus']
+// cars.unshift('KIA')
+// console.log(cars)
+
+// 12
+//
+// let cars = ['bmw', 'honda','mers','lexus']
+//
+// console.log(cars.length)
+
+
+
+
